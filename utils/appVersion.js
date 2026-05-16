1f700d040a6b9bdd63541a49973a54aa69b31b83
heimenkyou <wenbin.lo@outlook.com>
2026-05-17 03:25:00 +0800

    feat: 更新版本号v2.1.2

--- utils/appVersion.js
+++ utils/appVersion.js
@@ -1,3 +1,3 @@
-export const APP_VERSION_NAME = 'v2.1.1';
-export const APP_VERSION_CODE = 211;
+export const APP_VERSION_NAME = 'v2.1.2';
+export const APP_VERSION_CODE = 212;
 export const APP_DATA_VERSION = String(APP_VERSION_CODE);
