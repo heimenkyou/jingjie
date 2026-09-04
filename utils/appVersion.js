5001bae0754479871427ea1759052d728917b0df
heimenkyou <wenbin.lo@outlook.com>
2026-09-05 01:13:14 +0800

    chore: 发布 v2.2.0

--- utils/appVersion.js
+++ utils/appVersion.js
@@ -1,3 +1,3 @@
-export const APP_VERSION_NAME = 'v2.1.2';
-export const APP_VERSION_CODE = 212;
+export const APP_VERSION_NAME = 'v2.2.0';
+export const APP_VERSION_CODE = 220;
 export const APP_DATA_VERSION = String(APP_VERSION_CODE);
